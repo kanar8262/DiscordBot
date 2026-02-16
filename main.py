f994ac1d272f396ec681a58f1caaa4912bdc4541
kanar8262 <kanar8262@gmail.com>
2026-02-16 01:28:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import discord
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix="!")
-
-@bot.event
-async def on_ready():
-    print(f"Bot ist online als {bot.user}")
-
-bot.run("MTQ3MjcxMzc3ODUzNjc3NTc2MA.GTdfM1.Hl4ZRBxM2qGO7wGsepWz4OSzXigre29QC8wy8o")
 
