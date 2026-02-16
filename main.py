b983a8ec76854cf41e2239c141b3c2748dcc39cc
kanar8262 <kanar8262@gmail.com>
2026-02-16 01:31:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+import discord
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot ist online als {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
